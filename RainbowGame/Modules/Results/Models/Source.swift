da4d2d64c860603c6fe393081d33353446941d81
Nikita Kornuk <kornyk62@gmail.com>
2023-11-14 21:01:36 +0100

    add mock data

--- RainbowGame/Modules/Results/Models/Source.swift
+++ RainbowGame/Modules/Results/Models/Source.swift
@@ -5,4 +5,20 @@
 //  Created by Admin on 13.11.2023.
 //
 
-import Foundation
+import UIKit
+
+struct Statistick{
+    let gameNomer: String
+    let time:String
+}
+
+struct Source{
+    static func makeStatistick() -> [Statistick]{
+        [
+            .init(gameNomer: "1", time: "21.0"),
+            .init(gameNomer: "2", time: "5.0"),
+            .init(gameNomer: "3", time: "13.0"),
+            .init(gameNomer: "4", time: "11.0"),
+        ]
+    }
+}
