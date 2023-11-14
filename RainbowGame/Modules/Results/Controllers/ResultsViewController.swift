dd346aae6a12072d8b3fa282f3165e7bd0b12434
Nikita Kornuk <kornyk62@gmail.com>
2023-11-14 18:05:48 +0100

    add new fail, add button

--- RainbowGame/Modules/Results/Controllers/ResultsViewController.swift
+++ RainbowGame/Modules/Results/Controllers/ResultsViewController.swift
@@ -6,24 +6,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class ResultsViewController: UIViewController {
+    
+    private let cleanButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Очистить статистику", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+        setupViews()
+        makeCostrains()
+    }
+
+    func setupViews(){
+        view.addSubview(cleanButton)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func makeCostrains(){
+        cleanButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.leading.equalToSuperview().offset(53)
+            make.trailing.equalToSuperview().inset(53)
+            make.height.equalTo(63)
+        }
     }
-    */
-
 }
---
+++ RainbowGame/Modules/Results/Models/Source.swift
@@ -0,0 +1,8 @@
+//
+//  Source.swift
+//  RainbowGame
+//
+//  Created by Admin on 13.11.2023.
+//
+
+import Foundation
