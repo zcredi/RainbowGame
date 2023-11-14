ab69750c3600e1dcd5ecb6bcde4f784239e19bd7
Piohi <box1234567b@gmail.com>
2023-11-14 17:05:28 +0300

    Revert "timer + moreReadebleFonts"
    This reverts commit 51be2e991f5c57605986215d2f3d55c5634f8d68.

--- RainbowGame/Modules/Game/Controllers/GameViewController.swift
+++ RainbowGame/Modules/Game/Controllers/GameViewController.swift
@@ -10,10 +10,9 @@ import UIKit
 class GameViewController: UIViewController {
     let backButtonPic = UIImage(named: "backButton")
     let pauseButtonPic = UIImage(named: "pauseButton")
-    var timer = Timer()
     
     let wordOfGame = ["красный", "оранжевый", "жёлтый", "зелёный", "голубой", "синий", "фиолетовый"]
-    let colorsOfGame = [UIColor.red, UIColor.orange, UIColor.systemYellow, UIColor.systemGreen, UIColor.systemBlue, UIColor.blue, UIColor.purple]
+    let colorsOfGame = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.systemBlue, UIColor.blue, UIColor.purple]
     
     private lazy var textWithoutBackground: UILabel = {
         var label = UILabel()
@@ -23,7 +22,7 @@ class GameViewController: UIViewController {
         label.textColor = colorsOfGame.randomElement()
 //MARK: функция модели получить рандомный цвет
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.font = UIFont.systemFont(ofSize: 20)
         
         return label
     }()
@@ -38,7 +37,7 @@ class GameViewController: UIViewController {
         label.layer.masksToBounds = true
         label.backgroundColor = colorsOfGame.randomElement()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.font = UIFont.systemFont(ofSize: 20)
         
         return label
     }()
@@ -52,16 +51,13 @@ class GameViewController: UIViewController {
         return button
     }()
     
-    var time = 65
-    
     private lazy var labelTimer: UILabel = {
         var label = UILabel()
-        label.text = "\(makeTimeString(seconds: time))"
+        label.text = "00.06"
         label.numberOfLines = 0
         label.textColor = .black
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.monospacedSystemFont(ofSize: 30, weight: UIFont.Weight.regular)
-        // такой шрифт чтоб таймер не дергался при обновлении когда цифры имеют разную ширину
+        label.font = UIFont.systemFont(ofSize: 30)
         
         return label
     }()
@@ -75,7 +71,7 @@ class GameViewController: UIViewController {
     }()
 
     private func setView() {
-        view.backgroundColor = UIColor.systemGray5
+        view.backgroundColor = UIColor.gray
     }
     
     private func setupBackButton() {
@@ -98,47 +94,34 @@ class GameViewController: UIViewController {
         pauseButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
         pauseButton.bottomAnchor.constraint(equalTo: backButton.bottomAnchor).isActive = true
         pauseButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
-        pauseButton.addTarget(self, action: #selector(deleteTimer), for: .touchUpInside)
+
+        //backButton.addTarget(self, action: #selector(), for: .touchUpInside)
     }
     
-    private func setupLabelText1() {
+    func setupLabelText1() {
         textWithoutBackground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
         textWithoutBackground.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
 //        textWithBackground.leadingAnchor.constraint(equalTo: backButton.trailingAnchor).isActive = true
 //        textWithBackground.trailingAnchor.constraint(equalTo: pauseButton.leadingAnchor).isActive = true
        
     }
-    private func setupLabelText2() {
+    func setupLabelText2() {
         textWithBackground.textAlignment = .center
-        textWithBackground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 90).isActive = true
+        textWithBackground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 70).isActive = true
         textWithBackground.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         textWithBackground.heightAnchor.constraint(equalToConstant: 40).isActive = true
         textWithBackground.widthAnchor.constraint(equalToConstant: 205).isActive = true
         
     }
     
-    private func createTimer() {
-        timer = Timer.scheduledTimer(
-            timeInterval: 1,
-            target: self,
-            selector: #selector(tickTimer),
-            userInfo: nil,
-            repeats: true)
-    }
-    @objc private func deleteTimer() {
-        timer.invalidate()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         view.addSubview(backButton)
         view.addSubview(labelTimer)
         view.addSubview(pauseButton)
         view.addSubview(textWithoutBackground)
         view.addSubview(textWithBackground)
-        
         
         
         setView()
@@ -149,44 +132,7 @@ class GameViewController: UIViewController {
         setupLabelText2()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        createTimer()
-    }
-    func makeTimeString(seconds: Int) -> String {
-        var timeString = ""
-        timeString += String(format: "%02d", seconds / 60)
-        timeString += ":"
-        timeString += String(format: "%02d", seconds % 60)
-        return timeString
-    }
     
-    @objc func tickTimer() {
-        time -= 1
-        textWithBackground.backgroundColor = colorsOfGame.randomElement()
-        if time == 0 {
-            timer.invalidate()
-        }
-//        makeTimeString(seconds: time)
-        
-        labelTimer.text = "\(makeTimeString(seconds: time))"
-        
-//        var minutes = time / 60
-//        var seconds = time % 60
-//        if minutes > 10 {
-//            if seconds > 10 {
-//                labelTimer.text = "\(minutes):\(seconds)"
-//            } else {
-//                labelTimer.text = "\(minutes):0\(seconds)"
-//            }} else {
-//                if seconds > 10 {
-//                    labelTimer.text = "0\(minutes):\(seconds)"
-//                } else {
-//                    labelTimer.text = "0\(minutes):0\(seconds)"
-//                }
-//            }
-//        "\(time / 60):\(time % 60)"
-    }
 
     /*
     // MARK: - Navigation
