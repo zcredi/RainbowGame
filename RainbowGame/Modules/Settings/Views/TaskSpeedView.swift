c452b6cfda64779d88f27fe60f5c0555f04c3f6c
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2023-11-16 21:23:55 +0200

    SettingsViewsController, SettingsViews creation

---
+++ RainbowGame/Modules/Settings/Views/TaskSpeedView.swift
@@ -0,0 +1,82 @@
+//
+//  TaskSpeedView.swift
+//  RainbowGame
+//
+//  Created by Andrei Shpartou on 15/11/2023.
+//
+
+import UIKit
+
+class TaskSpeedView: UIView {
+    private enum Constants {
+        static let taskSpeedLabelSideSpace: CGFloat = 5.0
+        static let taskSpeedSliderTrailingSpace: CGFloat = -40.0
+        static let taskSpeedSliderLeadingSpace: CGFloat = 140.0
+        static let taskSpeedResultLabelTrailingSpace: CGFloat = -10.0
+    }
+    
+    private lazy var taskSpeedLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15)
+        label.text = "скорость смены\nзаданий, сек"
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var taskSpeedSlider: UISlider = {
+        let slider = UISlider()
+        slider.minimumValue = 1
+        slider.maximumValue = 6
+        slider.value = 3
+        slider.tintColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        slider.isContinuous = true
+        slider.addTarget(nil, action: #selector(SettingsViewController.taskSpeedSliderChanged), for: .valueChanged)
+        return slider
+    }()
+    
+    var taskSpeedResultLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 20)
+        label.text = "3"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        addSubview(taskSpeedLabel)
+        addSubview(taskSpeedSlider)
+        addSubview(taskSpeedResultLabel)
+    }
+    
+    private func setConstraints() {
+        taskSpeedLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            taskSpeedLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            taskSpeedLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.taskSpeedLabelSideSpace),
+            taskSpeedLabel.trailingAnchor.constraint(equalTo: taskSpeedSlider.leadingAnchor, constant: -Constants.taskSpeedLabelSideSpace)
+        ])
+        taskSpeedSlider.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            taskSpeedSlider.centerYAnchor.constraint(equalTo: centerYAnchor),
+            taskSpeedSlider.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.taskSpeedSliderLeadingSpace),
+            taskSpeedSlider.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Constants.taskSpeedSliderTrailingSpace)
+        ])
+        taskSpeedResultLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            taskSpeedResultLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            taskSpeedResultLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Constants.taskSpeedResultLabelTrailingSpace),
+        ])
+
+    }
+}
